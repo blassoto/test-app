61cfe1d3e36545fa60fd31142769ffed9dc0ab78
Blas Soto <blas.soto@pjchile.com>
2021-08-20 09:00:44 -0400

    query

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def index
+    result = ActiveRecord::Base.connection.execute('SELECT * FROM information_schema.tables')
+    @table_data = result.to_a.first
   end
 end
